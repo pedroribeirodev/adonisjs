be5c83e58f91ce3f92fbe562cf8e6e790d0c9ec5
pedroribeirodev <pedro.bonfim.13@gmail.com>
2019-11-10 10:34:01 -0300

    Alterações para usar fila no email, e cruds completos

---
+++ app/Exceptions/Handler.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const Sentry = require('@sentry/node')
+
+const Env = use('Env')
+const Youch = use('youch')
+const BaseExceptionHandler = use('BaseExceptionHandler')
+
+/**
+ * This class handles all exceptions thrown during
+ * the HTTP request lifecycle.
+ *
+ * @class ExceptionHandler
+ */
+class ExceptionHandler extends BaseExceptionHandler {
+  /**
+   * Handle exception thrown during the HTTP lifecycle
+   *
+   * @method handle
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   * @param  {Object} options.response
+   *
+   * @return {void}
+   */
+  async handle (error, { request, response }) {
+    if (error.name === 'ValidationException') {
+      return response.status(error.status).send(error.messages)
+    }
+
+    if (Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request)
+      const errorJSON = await youch.toJSON()
+
+      return response.status(error.status).send(errorJSON)
+    }
+
+    return response.status(error.status)
+  }
+
+  /**
+   * Report exception for logging or debugging.
+   *
+   * @method report
+   *
+   * @param  {Object} error
+   * @param  {Object} options.request
+   *
+   * @return {void}
+   */
+  async report (error, { request }) {
+    Sentry.init({ dsn: 'https://e74373fdd6074065b232be821db7cd96@sentry.io/1814693' })
+    Sentry.captureException(error)
+  }
+}
+
+module.exports = ExceptionHandler
